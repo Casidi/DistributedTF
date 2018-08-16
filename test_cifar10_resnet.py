ab4593f515d2619292fb5dca594eb8f70738dda8
tcfsh22215 <tcfsh22215@gmai.com>
2018-08-16 17:12:00 +0800

    Test the learning curve of cifar10 model

--- test_cifar10_resnet.py
+++ test_cifar10_resnet.py
@@ -1,5 +1,8 @@
 import sys
+import os
 sys.path.insert(0, './resnet')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 #NOTE: tensorflow >= 1.10 required
 from resnet import cifar10_main
@@ -13,15 +16,12 @@ hp = {
     'initializer': 'he_init',
     'batch_size': 128}
 
-model_id = 0
+model_id = 7
 save_base_dir = './resnet/model_'
 data_dir = '/home/K8S/dataset/cifar10'
 train_epochs = 1
 
-results = []
-eval_accuracy, model_id = \
-    cifar10_main.main(hp, model_id, save_base_dir, data_dir, train_epochs)
-print 'First run', (eval_accuracy, model_id)
-eval_accuracy, model_id = \
-    cifar10_main.main(hp, model_id+1, save_base_dir, data_dir, train_epochs)
-print 'Second run', (eval_accuracy, model_id)
+for i in range(50):
+    eval_accuracy, model_id = \
+        cifar10_main.main(hp, model_id, save_base_dir, data_dir, train_epochs)
+    print 'Epoch {}, acc = {}'.format(i+1, eval_accuracy)
