6879a334f864ec649ad02cb35bab1b3e6a1c6c21
JayHsieh1104 <jj011644@gmail.com>
2018-08-20 16:18:55 +0800

    Add perturb_hparams function

--- cifar10_model.py
+++ cifar10_model.py
@@ -1,4 +1,7 @@
+from constants import get_hp_range_definition
 import sys
+import random
+import numpy as np
 sys.path.insert(0, './resnet')
 
 #NOTE: tensorflow >= 1.10 required
@@ -32,7 +35,73 @@ class Cifar10Model:
         return
 
     def perturb_hparams(self):
-        #TODO: implement this function to get the exploring working
+        def _perturb_float(val, limit_min, limit_max):
+            # Noted, some hp value can't exceed reasonable range
+            float_string = str(limit_min)
+            if 'e' in float_string:
+                _, n_digits = float_string.split('e')
+                if '-' in n_digits:
+                    n_digits = int(n_digits) * -1
+                else:
+                    n_digits = int(n_digits)
+            else:
+                n_digits = str(limit_min)[::-1].find('.')
+            min = val * self._perturb_factors[0]
+            max = val * self._perturb_factors[1]
+            if min < limit_min:
+                min = limit_min
+                n_digits += 1
+            if max > limit_max:
+                max = limit_max
+            val = random.uniform(min, max)
+            val = round(val, n_digits)
+
+            return val
+
+        def _perturb_int(val, limit_min, limit_max):
+            # Noted, some hp value can't exceed reasonable range
+            if limit_min == limit_max:
+                limit_min = 0
+            min = int(np.floor(val * self._perturb_factors[0]))
+            max = int(np.ceil(val * self._perturb_factors[1]))
+            if min < limit_min:
+                min = limit_min
+            if max > limit_max:
+                max = limit_max
+            return random.randint(min, max)
+
+        range_def = get_hp_range_definition()
+        for key, value in self.hparams.iteritems():
+            if isinstance(value, float):
+                self.hparams[key] = _perturb_float(value, range_def[key][0], range_def[key][-1])
+            elif isinstance(value, int):
+                self.hparams[key] = _perturb_int(value, range_def[key][0], range_def[key][-1])
+            else:
+                if key != 'opt_case':
+                    # Model-architecture related HP is kept the same
+                    if key == 'num_filters_1' or key == 'kernel_size_1' \
+                            or key == 'kernel_size_2' or key == 'activation' \
+                            or key == 'initializer':
+                        pass
+                    else:
+                        self.hparams[key] = random.choice(range_def[key])
+                else:
+                    # Notes, hparams[key]['optimizer'] is kept the same
+                    optimizer = self.hparams[key]['optimizer']
+                    self.hparams[key]['lr'] = \
+                        _perturb_float(self.hparams[key]['lr'], \
+                                       range_def['lr'][optimizer][0], \
+                                       range_def['lr'][optimizer][-1])
+
+                    if self.hparams[key]['optimizer'] == 'Momentum' \
+                            or self.hparams[key]['optimizer'] == 'RMSProp':
+                        self.hparams[key]['momentum'] = \
+                            _perturb_float(self.hparams[key]['momentum'], \
+                                           range_def['momentum'][0], range_def['momentum'][-1])
+                    if self.hparams[key]['optimizer'] == 'RMSProp':
+                        self.hparams[key]['grad_decay'] = \
+                            _perturb_float(self.hparams[key]['grad_decay'], \
+                                           range_def['grad_decay'][0], range_def['grad_decay'][-1])
         return
 
     def get_accuracy(self):
